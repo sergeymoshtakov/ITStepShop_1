c2d242cc0d1684fbba4c7bf6ba4597ce57e7c85d
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-02-20 13:10:19 +0100

    migrations added

--- Migrations/20240220120937_AddProductUsageToProduct.Designer.cs
+++ Migrations/20240220120937_AddProductUsageToProduct.Designer.cs
@@ -2,6 +2,7 @@
 using ITStepShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ITStepShop.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240220120937_AddProductUsageToProduct")]
+    partial class AddProductUsageToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240220120937_AddProductUsageToProduct.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ITStepShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductUsageToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductUsageId",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ProductUsage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductUsage", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_ProductUsageId",
+                table: "Product",
+                column: "ProductUsageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_ProductUsage_ProductUsageId",
+                table: "Product",
+                column: "ProductUsageId",
+                principalTable: "ProductUsage",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_ProductUsage_ProductUsageId",
+                table: "Product");
+
+            migrationBuilder.DropTable(
+                name: "ProductUsage");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_ProductUsageId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "ProductUsageId",
+                table: "Product");
+        }
+    }
+}
